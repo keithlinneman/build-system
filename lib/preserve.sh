27967c856ba40911559c613df8681bc4d3af9202
Keith Linneman <keith@linnemanlabs.com>
2026-01-29 02:45:25 -0500

    pulling components from build context

--- lib/preserve.sh
+++ lib/preserve.sh
@@ -1,14 +1,17 @@
 # shellcheck shell=bash
 
 preserve_audit_artifacts() {
-  local s3_path="s3://${DEPLOYMENT_BUCKET}/apps/${APP}/releases/${RELEASE_VERSION}/${BUILD_ID}/"
-  echo "==> (preserve-audit) Uploading release to S3: ${s3_path}"
-  aws s3 cp --recursive "${DIST}/" "${s3_path}"
+  for component in $( ctx_list_components );do
+    local s3_path="s3://${DEPLOYMENT_BUCKET}/apps/${APP}/${component}/releases/${RELEASE_ID}/"
+    echo "==> (preserve-audit) Uploading component=${component} release to S3: ${s3_path}"
+    aws s3 cp --recursive "${DIST}/${component}" "${s3_path}"
+  done
 }
 
 list_audit_artifacts() {
-  local s3_path="s3://${DEPLOYMENT_BUCKET}/apps/${APP}/releases/${RELEASE_VERSION}/${BUILD_ID}/"
-  echo "==> (preserve-audit) Listing release contents in S3: ${s3_path}"
-  aws s3 ls --recursive --human-readable "${s3_path}"
+  for component in $( ctx_list_components );do
+    local s3_path="s3://${DEPLOYMENT_BUCKET}/apps/${APP}/${component}/releases/${RELEASE_ID}/"
+    echo "==> (preserve-audit) Listing release contents component=${component} in S3: ${s3_path}"
+    aws s3 ls --recursive --human-readable "${s3_path}"
+  done
 }
-
